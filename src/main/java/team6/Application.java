4c042dfad795101e2a98cf0a7ff96151375c49e6
AleAngheben <144798020+AleAngheben@users.noreply.github.com>
2024-01-29 16:25:14 +0100

    tickets classes done ( miss relations )

--- src/main/java/team6/Application.java
+++ src/main/java/team6/Application.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Application {
-    private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory("DB_traporto_pubblico");
+    private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory("u4bw");
     public static EntityManager em =  emf.createEntityManager();
     public static void main(String[] args) {
         System.out.println("Hello World!");
