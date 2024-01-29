7b32aedb26010ba0378957ced31a694100e40893
abdHar12 <145681190+abdHar12@users.noreply.github.com>
2024-01-29 11:01:23 +0100

    commit

--- src/main/java/team6/Application.java
+++ src/main/java/team6/Application.java
@@ -1,7 +1,12 @@
 package team6;
 
-public class Application {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class Application {
+    private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory("DB_traporto_pubblico");
+    public static EntityManager em =  emf.createEntityManager();
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
